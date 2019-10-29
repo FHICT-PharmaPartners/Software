9a9072339e2cf154f4392995f9369275773cdcaa
RobinvandenHurk <robinvandenhurk@live.nl>
2019-10-29 15:17:32 +0100

    Fixed issues + added endpoint function

--- mijnpharmaRESTServer/src/main/java/nl/pharmapartners/mypharma/restserver/controllers/UserController.java
+++ mijnpharmaRESTServer/src/main/java/nl/pharmapartners/mypharma/restserver/controllers/UserController.java
@@ -1,11 +1,11 @@
 package nl.pharmapartners.mypharma.restserver.controllers;
 
-import nl.pharmapartners.mypharma.library.model.Patient;
+import com.google.gson.Gson;
+import nl.pharmapartners.mypharma.library.bll.UserRepository;
+import nl.pharmapartners.mypharma.library.model.User;
+import nl.pharmapartners.mypharma.restserver.model.MedicalInfo;
 import org.springframework.http.MediaType;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-
-import javax.websocket.server.PathParam;
 
 @RestController
 @RequestMapping("/user")
@@ -19,24 +19,27 @@ public class UserController {
 
     //Get request for single user
     @GetMapping(value = "/{id}")
-    public void getUserById(@PathParam("id") int id) {
+    public String getUserById(@PathVariable("id") int id) {
+        User user = new UserRepository().getUserById(id);
+        String response = new Gson().toJson(user);
 
+        return response;
     }
 
     //Put request to update user information
-    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
-    public void updateUserInformation(Patient patient) {
+    @PutMapping(value = "/{id}/updateUserInfo", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public void updateUserInformation(@PathVariable("id") String id, @ModelAttribute MedicalInfo info) {
 
     }
 
     //Post request to log the user in
-    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(value = "login", consumes = MediaType.APPLICATION_JSON_VALUE)
     public void logUserIn(String emailAddress, String password) {
 
     }
 
     //Delete request to log the user out
-    @DeleteMapping()
+    @PostMapping(value = "logout")
     public void logUserOut() {
 
     }
