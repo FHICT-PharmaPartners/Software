b397c2d8895c1ca9532c099efb35f74c68e59598
iTouchi <aytacsarkmaz@gmail.com>
2019-11-05 12:58:13 +0100

    ATC added to ATCRULE

--- mijnpharmaLibrary/src/main/java/nl/pharmapartners/mypharma/library/model/ATCRule.java
+++ mijnpharmaLibrary/src/main/java/nl/pharmapartners/mypharma/library/model/ATCRule.java
@@ -1,8 +1,16 @@
 package nl.pharmapartners.mypharma.library.model;
 
 public class ATCRule {
+
     private int id;
     private String ATCCheck;
+    private ATC atc;
+
+    public ATCRule(int id, String ATCCheck, ATC atc) {
+        this.id = id;
+        this.ATCCheck = ATCCheck;
+        this.atc = atc;
+    }
 
     public int getId() {
         return id;
@@ -18,5 +26,13 @@ public class ATCRule {
 
     public void setATCCheck(String ATCCheck) {
         this.ATCCheck = ATCCheck;
+    }
+
+    public ATC getAtc() {
+        return atc;
+    }
+
+    public void setAtc(ATC atc) {
+        this.atc = atc;
     }
 }
