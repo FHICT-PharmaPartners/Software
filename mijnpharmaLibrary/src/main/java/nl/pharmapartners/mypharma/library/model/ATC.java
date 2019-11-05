70b0a94b8ab1ecbc36cc61d9d8053ff6eba87b30
iTouchi <aytacsarkmaz@gmail.com>
2019-11-05 12:55:22 +0100

    ATC class added

--- mijnpharmaLibrary/src/main/java/nl/pharmapartners/mypharma/library/model/ATC.java
+++ mijnpharmaLibrary/src/main/java/nl/pharmapartners/mypharma/library/model/ATC.java
@@ -1,5 +1,38 @@
 package nl.pharmapartners.mypharma.library.model;
 
 public class ATC {
-    //<3 robin
+
+    int id;
+    String type;
+    String name;
+
+    public ATC(int id, String type, String name) {
+        this.id = id;
+        this.type = type;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
