e945f36a2ae11a9a7a12cf1b272a0c8423ad4cfa
iTouchi <aytacsarkmaz@gmail.com>
2019-11-05 13:46:01 +0100

    MedicineREpository added

--- mijnpharmaLibrary/src/main/java/nl/pharmapartners/mypharma/library/bll/MedicineRepository.java
+++ mijnpharmaLibrary/src/main/java/nl/pharmapartners/mypharma/library/bll/MedicineRepository.java
@@ -3,13 +3,37 @@ package nl.pharmapartners.mypharma.library.bll;
 import nl.pharmapartners.mypharma.library.dal.database.MySQLMedicineContext;
 import nl.pharmapartners.mypharma.library.model.Medicine;
 
+import javax.swing.plaf.metal.MetalComboBoxEditor;
 import java.util.ArrayList;
 
 public class MedicineRepository {
 
-    MySQLMedicineContext medicineContext = new MySQLMedicineContext();
+    private ArrayList<Medicine> medicine;
+    private MySQLMedicineContext context;
 
-    public ArrayList<Medicine> getAllMedicine() {
-        return medicineContext.getAllMedicine();
+    public MedicineRepository(){
+        context = new MySQLMedicineContext();
     }
+
+    public  ArrayList<Medicine> getAllMedicine(){
+        if (medicine == null){
+            medicine = context.getAllMedicine();
+        }
+
+        // Return a copy of the array
+        return new ArrayList<>(medicine);
+    }
+
+    public Medicine getMedicineById(int id){
+        return  context.getMedicineById(id);
+    }
+
+    private MySQLMedicineContext getContext(){
+        if (context == null) {
+            context = new MySQLMedicineContext();
+        }
+
+        return context;
+    }
+
 }
