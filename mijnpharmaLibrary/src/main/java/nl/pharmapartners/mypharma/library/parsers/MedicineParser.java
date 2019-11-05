3be266e28b11dff2b64ac7ac12a5bf97b3a7c556
iTouchi <aytacsarkmaz@gmail.com>
2019-11-05 13:37:57 +0100

    MedicineParser added

--- mijnpharmaLibrary/src/main/java/nl/pharmapartners/mypharma/library/parsers/MedicineParser.java
+++ mijnpharmaLibrary/src/main/java/nl/pharmapartners/mypharma/library/parsers/MedicineParser.java
@@ -3,19 +3,25 @@ package nl.pharmapartners.mypharma.library.parsers;
 import nl.pharmapartners.mypharma.library.model.Medicine;
 
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class MedicineParser {
 
-    public Medicine fromResulteSet(ResultSet rs){
+    public Medicine fromResulteSet(ResultSet rs) {
         Medicine medicine = null;
 
-        try{
-            if (rs.next()){
+        try {
+            if (rs.next()) {
                 int id = rs.getInt("id");
                 String name = rs.getString("name");
                 String description = rs.getString("description");
 
+                medicine = new Medicine(id, name, description);
             }
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
+
+        return medicine;
     }
 }
