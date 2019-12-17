1f3aca15f3a68eb80cf68ecc1932843d1771bf17
RobinvandenHurk <robinvandenhurk@live.nl>
2019-12-17 12:54:13 +0100

    Added authentication

--- mijnpharmaLibrary/src/main/java/nl/pharmapartners/mypharma/library/dal/repository/MedicineRepository.java
+++ mijnpharmaLibrary/src/main/java/nl/pharmapartners/mypharma/library/dal/repository/MedicineRepository.java
@@ -4,9 +4,7 @@ import nl.pharmapartners.mypharma.library.model.Medicine;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface MedicineRepository extends JpaRepository<Medicine, String> {
-    List<Medicine> findByMedicineContaining(String string);
+
 }
