6aa351c6d394c26991ab39be30798fe032f2db7f
Robin van den Hurk <24567349+RobinvandenHurk@users.noreply.github.com>
2019-12-23 13:11:11 +0100

    Update MedicineRepository.java

--- mijnpharmaLibrary/src/main/java/nl/pharmapartners/mypharma/library/dal/repository/MedicineRepository.java
+++ mijnpharmaLibrary/src/main/java/nl/pharmapartners/mypharma/library/dal/repository/MedicineRepository.java
@@ -4,8 +4,9 @@ import nl.pharmapartners.mypharma.library.model.Medicine;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
 public interface MedicineRepository extends JpaRepository<Medicine, String> {
-    
-    
+
+
 }
