20df3da6941c5f82cd49deecefa344169ec2df68
Robin van den Hurk <24567349+RobinvandenHurk@users.noreply.github.com>
2019-12-23 13:12:19 +0100

    Update PrkRepository.java

--- mijnpharmaLibrary/src/main/java/nl/pharmapartners/mypharma/library/dal/repository/PrkRepository.java
+++ mijnpharmaLibrary/src/main/java/nl/pharmapartners/mypharma/library/dal/repository/PrkRepository.java
@@ -1,9 +1,9 @@
 package nl.pharmapartners.mypharma.library.dal.repository;
 
-import nl.pharmapartners.mypharma.library.model.PRKRule;
+import nl.pharmapartners.mypharma.library.model.Prk;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PRKRepository extends JpaRepository<PRKRule, String> {
+public interface PRKRepository extends JpaRepository<Prk, String> {
 }
