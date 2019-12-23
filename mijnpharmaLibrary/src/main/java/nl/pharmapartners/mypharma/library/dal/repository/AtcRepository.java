4741696e89cb765c8375da7ba00873e7127934b6
Robin van den Hurk <24567349+RobinvandenHurk@users.noreply.github.com>
2019-12-23 13:10:07 +0100

    Update AtcRepository.java

--- mijnpharmaLibrary/src/main/java/nl/pharmapartners/mypharma/library/dal/repository/AtcRepository.java
+++ mijnpharmaLibrary/src/main/java/nl/pharmapartners/mypharma/library/dal/repository/AtcRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ATCRepository extends JpaRepository<Atc, String> {
+public interface AtcRepository extends JpaRepository<Atc, String> {
 }
